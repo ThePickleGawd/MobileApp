4049e5aad4c60fa4020bfc53e962c49438526c7d
Dylan Lu <dylanelu@gmail.com>
2021-08-31 20:07:21 -0700

    school stuff

--- App.tsx
+++ App.tsx
@@ -1,21 +1,36 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import {
+  Box,
+  Center,
+  Heading,
+  NativeBaseProvider,
+  HStack,
+  VStack,
+  Flex,
+  Divider,
+  Icon,
+} from "native-base";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NativeBaseProvider>
+      <Box safeAreaTop alignItems="center" flex={1} backgroundColor="lightblue">
+        <Heading>Mobile App</Heading>
+        <VStack space={3}>
+          <HStack justifyContent="space-between">
+            <Text>Simon Mignolet</Text>
+            <Text>Test</Text>
+          </HStack>
+          <HStack justifyContent="space-between">
+            <Text>Nathaniel Clyne</Text>
+          </HStack>
+          <HStack justifyContent="space-between">
+            <Text>Dejan Lovren</Text>
+          </HStack>
+        </VStack>
+      </Box>
+    </NativeBaseProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
