1e49b466ecd78d6b02cac336baa045bbbf8d8042
Dylan Lu <42813970+ThePickleGawd@users.noreply.github.com>
2021-08-31 22:34:27 -0700

    playing around

--- App.tsx
+++ App.tsx
@@ -11,25 +11,26 @@ import {
   Flex,
   Divider,
   Icon,
+  Stack,
 } from "native-base";
 
 export default function App() {
   return (
     <NativeBaseProvider>
-      <Box safeAreaTop alignItems="center" flex={1} backgroundColor="lightblue">
-        <Heading>Mobile App</Heading>
-        <VStack space={3}>
-          <HStack justifyContent="space-between">
-            <Text>Simon Mignolet</Text>
-            <Text>Test</Text>
-          </HStack>
-          <HStack justifyContent="space-between">
-            <Text>Nathaniel Clyne</Text>
-          </HStack>
-          <HStack justifyContent="space-between">
-            <Text>Dejan Lovren</Text>
-          </HStack>
-        </VStack>
+      <Box safeAreaTop flex={1} backgroundColor="lightblue">
+        <Center>
+          <Heading>Mobile App</Heading>
+        </Center>
+        <Stack flex={1} space={3}>
+          <Flex direction="row" flex={1}>
+            <Box backgroundColor="red" rounded="lg" flex={1}></Box>
+            <Box backgroundColor="blue" rounded="lg" flex={1}></Box>
+          </Flex>
+          <Flex direction="row" flex={1}>
+            <Box backgroundColor="green" rounded="lg" flex={1}></Box>
+            <Box backgroundColor="purple" rounded="lg" flex={1}></Box>
+          </Flex>
+        </Stack>
       </Box>
     </NativeBaseProvider>
   );
